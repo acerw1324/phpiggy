5a3c11f4af176488b59514a27a88dfe48787449f
acerw1324 <lameiro.alejandro@gmail.com>
2024-12-16 15:55:27 -0500

    Update index.php

--- src/App/views/index.php
+++ src/App/views/index.php
@@ -1,4 +1,4 @@
-<?php include ""; ?>
+<?php include $this->resolve("partials/_header.php"); ?>
 
 <!-- Start Main Content Area -->
 <section class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
@@ -260,11 +260,4 @@
 </section>
 <!-- End Main Content Area -->
 
-<!-- Footer -->
-<footer class="container mx-auto my-12 text-center">
-    &copy; PHPiggy
-</footer>
-<!-- End Footer -->
-</body>
-
-</html>
+<?php include $this->resolve("partials/_footer.php"); ?>
