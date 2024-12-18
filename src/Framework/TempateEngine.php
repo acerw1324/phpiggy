83ec17e8acc76aa8066947e8dee6f274a4c94945
acerw1324 <lameiro.alejandro@gmail.com>
2024-12-18 16:09:00 -0500

    Update TempateEngine.php

--- src/Framework/TempateEngine.php
+++ src/Framework/TempateEngine.php
@@ -6,11 +6,14 @@ namespace Framework;
 
 class TemplateEngine
 {
+    private array $globalTemplateData = [];
+
     public function __construct(private string $basePath) {}
 
     public function render(string $template, array $data = [])
     {
         extract($data, EXTR_SKIP);
+        extract($this->globalTemplateData);
 
         ob_start();
 
