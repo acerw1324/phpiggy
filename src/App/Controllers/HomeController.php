fadae70a284a4a57a2ea9fac25594cbe2aa5483d
acerw1324 <lameiro.alejandro@gmail.com>
2025-01-08 16:04:26 -0500

    Update HomeController.php

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -18,6 +18,6 @@ class HomeController
 
     public function home()
     {
-        $this->view->render("index.php", ['title' => 'Home page']);
+        $this->view->render("index.php", ['title' => 'Home Page']);
     }
 }
