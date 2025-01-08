1cce8b690b8a963ec77ad9bda0de44f0a04da06f
acerw1324 <lameiro.alejandro@gmail.com>
2025-01-08 15:40:56 -0500

    Update TemplateEngine.php

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -8,7 +8,7 @@ class TemplateEngine
 {
     public function __construct(private string $basePath) {}
 
-    public function render(string $template)
+    public function render(string $template, array $data = [])
     {
         include "{$this->basePath}/{$template}";
     }
