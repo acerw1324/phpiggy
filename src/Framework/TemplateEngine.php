02923ef5ac7cf901f31f9f11eb9993d4c0cab42a
acerw1324 <lameiro.alejandro@gmail.com>
2025-04-29 19:57:54 -0400

    Update TemplateEngine.php

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -6,6 +6,8 @@ namespace Framework;
 
 class TemplateEngine
 {
+    private array $globalTemplateData = [];
+
     public function __construct(private string $basePath) {}
 
     public function render(string $template, array $data = [])
