5a62a78db68524fc3ce7441d84ff08070eea5e0d
acerw1324 <lameiro.alejandro@gmail.com>
2025-04-30 18:14:04 -0400

    Update about.php

--- src/App/views/about.php
+++ src/App/views/about.php
@@ -1,7 +1,8 @@
 <?php include $this->resolve("partials/_header.php"); ?>
 
 <!-- Start Main Content Area -->
-<section class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
+<section
+    class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
     <!-- Page Title -->
     <h3>About Page</h3>
 
