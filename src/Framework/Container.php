2bdb2418d19f51fd29901e2c5d23153e63ae0af7
acerw1324 <lameiro.alejandro@gmail.com>
2025-05-01 17:00:37 -0400

    Update Container.php

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,12 +4,4 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class Container
-{
-    private array $definitions = [];
-
-    public function addDefinitions(array $newDefinitions)
-    {
-        dd($newDefinitions);
-    }
-}
+class Container {}
